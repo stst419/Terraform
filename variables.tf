03c13d0e2f17418932c44c802ee094f43127c004
stst419 <36875581+stst419@users.noreply.github.com>
2020-07-28 21:30:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "aws_access_key" {
-default = ""
+default = "AKIA23S7KTW6M6QFIARK"
 }
 variable "aws_secret_key" {
-default = ""
+default = "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"
 }
 variable "region" {
 default = "us-east-1"
